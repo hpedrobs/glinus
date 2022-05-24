36cc55d8f20affc4433737112904f924f6409091
PH <hpedrobs@gmail.com>
2022-05-24 18:00:08 -0300

    Update validation

--- src/core/validation.ts
+++ src/core/validation.ts
@@ -10,19 +10,19 @@ export default () : Promise<Boolean> => {
     return new Promise(resolve => {
         try {
             if (!process.env.USER || !process.env.PASSWORD) {
-                throw new Error('Error: You need to set the credentials to login to the site!')
+                throw new Error('You need to set the credentials to login to the site!')
             }
 
             if (!process.env.START_DATE || !process.env.END_DATE) {
-                throw new Error('Error: You need to set the period to carry out an inquiry!')
+                throw new Error('You need to set the period to carry out an inquiry!')
             }
 
             if (!process.env.NUM_IE && typeof process.env.NUM_IE === 'string') {
-                throw new Error('Error: You need to define a valid state registration to perform the query!')
+                throw new Error('You need to define a valid state registration to perform the query!')
             }
             resolve(true)
         } catch (error: any) {
-            clg(error.message)
+            clg(error.message, 'warn')
             resolve(false)
         }
     })
