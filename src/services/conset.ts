8aa2db95822cd43cf760b6ffa139e73157e1a36d
PH <hpedrobs@gmail.com>
2022-05-24 15:19:55 -0300

    Delete conset.ts

--- src/services/conset.ts
+++
@@ -1,7 +0,0 @@
-export default async (page: any): Promise<void> => {
-    try {
-        await page.locator('text=Concordo').click()
-    } catch (error: any) {
-        console.log(error)
-    }
-}
