f8eb1e7e719ab1b9757170854d21a3fa93ec11f1
PH <hpedrobs@gmail.com>
2022-05-24 17:59:53 -0300

    Create log types

--- src/core/clg.ts
+++ src/core/clg.ts
@@ -1,8 +1,49 @@
 /* external */
 import colors from 'colors'
 
-export default function (...params: any) {
+const success: any = colors.green
+const warn: any = colors.yellow
+const error: any = colors.red
+const info: any = colors.gray
+
+function setColor (text: string, type: string) : string {
+    switch (type) {
+    case 'success':
+        text = success(text)
+        break
+    case 'warn':
+        text = warn(text)
+        break
+    case 'info':
+        text = info(text)
+        break
+    case 'error':
+        text = error(text)
+        break
+    }
+
+    return text
+}
+
+function setTitleDescription (text: string, type: string) : string {
+    switch (type) {
+    case 'warn':
+        text = `warning: ${text}`
+        break
+    case 'error':
+        text = `error: ${text}`
+        break
+    case 'info':
+        text = `info: ${text}`
+        break
+    }
+
+    return text
+}
+
+export default function (text: string, type: string = '') : void {
     const name: string = colors.bgGreen.black('GLINUS')
     const time: string = colors.bold(new Date().toLocaleTimeString())
-    console.log('>', name, time, String(...params))
+    text = setColor(setTitleDescription(text, type), type)
+    console.log('>', name, time, text)
 }
