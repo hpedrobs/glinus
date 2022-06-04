085148fc445cb4fb8cc54084fa5a563808a8fab4
PH <hpedrobs.dev@gmail.com>
2022-06-04 19:59:36 -0300

    Update GoesThroughCaptcha.ts

--- src/services/GoesThroughCaptcha.ts
+++ src/services/GoesThroughCaptcha.ts
@@ -9,12 +9,14 @@ function timeOut (time: number): Promise<string> {
     })
 }
 
-const siteDetails = {
-    sitekey: '6LfTFzIUAAAAAKINyrQ9X5LPg4W3iTbyyYKzeUd3',
-    pageurl: 'https://nfe.sefaz.go.gov.br/nfeweb/sites/nfe/consulta-publica'
-}
+async function captcha (page: Page) {
+    const sitekey = await page.getAttribute('.g-recaptcha', 'data-sitekey') ?? ''
 
-async function captcha () {
+    const siteDetails = {
+        sitekey,
+        pageurl: 'https://nfe.sefaz.go.gov.br/nfeweb/sites/nfe/consulta-publica'
+    }
+
     const requestId = await initiateCaptchaRequest(siteDetails)
     const response = await pollForRequestResults(requestId)
     return response
@@ -23,7 +25,7 @@ async function captcha () {
 export default async (page: Page): Promise<Boolean> => {
     try {
         // time out 3 minutes if captcha not return results
-        const response = await Promise.race([captcha(), timeOut(180000)])
+        const response = await Promise.race([captcha(page), timeOut(180000)])
         if (response === 'TIME_EXCEED') throw new Error('TIME EXCEED - GOES THROUGH CAPTCHA')
 
         await page.evaluate(`document.getElementById("g-recaptcha-response").innerHTML="${response}";`)
